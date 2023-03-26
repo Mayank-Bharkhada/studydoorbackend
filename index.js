28bdfa09f2167aa5d96e080d228cdccaaee7bbc9
Mr. Mayank Bharkhada <94800249+Mayank-Bharkhada@users.noreply.github.com>
2023-03-26 14:34:37 +0530

    port

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.use('/api/User',require('./routes/Admin'));
 // app.use('/api/User',require('./routes/Institute_registration'));
 
 
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log(`Server is running at ${port} port : `);
 }); 
