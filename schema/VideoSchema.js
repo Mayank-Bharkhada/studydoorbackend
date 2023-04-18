580fb858d899f8f76eb7c277b7baf931f90f1438
Mr. Mayank Bharkhada <94800249+Mayank-Bharkhada@users.noreply.github.com>
2023-04-18 20:42:56 +0530

    new

---
+++ schema/VideoSchema.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const VideoSchema = new mongoose.Schema({
+    institute_id:{
+        type: Schema.Types.ObjectId,
+        ref: 'Institute',
+        required: true,
+      },
+      course: {
+        type: String,
+        required: true
+      },
+      department: {
+        type: String,
+        required: true, 
+      },
+      semester: {
+        type: Number,
+        required: true, 
+      },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  videoUrl: {
+    type: String,
+    required: true
+  },
+  thumbnailUrl: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const VideoModel = mongoose.model('Video', VideoSchema);
+
+module.exports = VideoModel;
