517acc61a7bfcf931920cbcae2ddf41b1b937d9a
Mr. Mayank Bharkhada <94800249+Mayank-Bharkhada@users.noreply.github.com>
2023-04-28 12:31:48 +0530

    new

--- routes/Student_portal.js
+++ routes/Student_portal.js
@@ -14,6 +14,7 @@ const EnrollmentModel = require('../schema/EnrollmentSchema');
 const BookModel = require('../schema/BookSchema');
 const VideoModel = require('../schema/VideoSchema');
 const QuestionModel = require('../schema/QuestionSchema');
+const LectureModel = require('../schema/LectureSchema');
 
 AWS.config.update({
   accessKeyId: "AKIAZKCVVG4RL7DOYPHL",
@@ -371,6 +372,38 @@ router.post('/fetch_all_videos_by_users_id', async (req, res) => {
       console.log(Enrollment.courseDepartment);
       console.log(Enrollment.courseName);
       const Videos = await VideoModel.find({institute_id: Enrollment.institute_id,course:Enrollment.courseName,department:Enrollment.courseDepartment});
+      console.log(Videos);
+       res.json([{
+          id: 1,
+          data: Videos,
+        }]);
+    }else{
+      res.json([{
+          id: 0,
+          data: 'No data',
+        }]);
+    }
+  } catch (error) { 
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+});
+
+
+
+//call through /api/User/fetch_all_lectures_by_users_id
+
+router.post('/fetch_all_lectures_by_users_id', async (req, res) => {
+  try {
+    
+    const Student_id = req.body.Student_id;
+
+    const Enrollment = await EnrollmentModel.findOne({student_id: Student_id,confirm: 1});
+    if(Enrollment){
+      console.log(Enrollment.institute_id);
+      console.log(Enrollment.courseDepartment);
+      console.log(Enrollment.courseName);
+      const Videos = await LectureModel.find({institute_id: Enrollment.institute_id,course:Enrollment.courseName,department:Enrollment.courseDepartment});
       console.log(Videos);
        res.json([{
           id: 1,
