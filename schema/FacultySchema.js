8d0883117b4cf99e10b529b5e4d785c1188131c7
Mr. Mayank Bharkhada <94800249+Mayank-Bharkhada@users.noreply.github.com>
2023-04-29 18:03:31 +0530

    new

---
+++ schema/FacultySchema.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the course schema
+const FacultySchema = new mongoose.Schema({
+  institute_id:{
+    type: Schema.Types.ObjectId,
+    ref: 'Institute',
+    required: true,
+    unique: false
+  },
+  fullName: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  course: {
+    type: String,
+    required: true
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  userUuid: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
+});
+
+
+// Create a Course model using the schema
+const FacultyModel = mongoose.model('Faculty', FacultySchema);
+module.exports = FacultyModel;
