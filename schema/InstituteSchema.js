6cef16e8dd9cb8bf66bccaff218a5a33edecd58c
Mr. Mayank Bharkhada <94800249+Mayank-Bharkhada@users.noreply.github.com>
2023-05-01 18:14:45 +0530

    new

--- schema/InstituteSchema.js
+++ schema/InstituteSchema.js
@@ -2,53 +2,90 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const InstituteSchema = new mongoose.Schema({
-  institute_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Institute',
-    required: true,
-    unique: false
-  },
-  fullName: {
-    type: String,
-    required: true
-  },
-  number: {
+  name: {
     type: String,
     required: true,
-    unique: true
-  },
-  course: {
-    type: String,
-    required: true
-  },
-  department: {
-    type: String,
-    required: true
-  },
-  userName: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  userUuid: {
-    type: String,
-    required: true,
-    unique: true
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
-  createdAt: {
+  dateOfEstablishment: {
     type: Date,
-    default: Date.now,
-    required: true
-  }
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: Number,
+    required: true,
+  },
+  accreditationCertificateUrl: {
+    type: String,
+    required: false,
+  },
+  businessRegistrationCertificateUrl: {
+    type: String,
+    required: false,
+  },
+  institutePhoto: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  verificationPhoto: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  block: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  verified: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  verificationRequest: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  UserName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  UserUuid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
 });
 
 const InstituteModel = mongoose.model("Institute", InstituteSchema);
