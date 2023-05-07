a99c1538b76ad50957ac3caab2d46e24d4ecf5cb
Mr. Mayank Bharkhada <94800249+Mayank-Bharkhada@users.noreply.github.com>
2023-05-07 20:51:13 +0530

    new

--- routes/Student_portal.js
+++ routes/Student_portal.js
@@ -406,10 +406,17 @@ router.post('/fetch_all_videos_by_users_id', async (req, res) => {
       console.log(Enrollment.courseName);
       const Videos = await VideoModel.find({institute_id: Enrollment.institute_id,course:Enrollment.courseName,department:Enrollment.courseDepartment});
       console.log(Videos);
-       res.json([{
+       if(Videos){
+        res.json([{
           id: 1,
           data: Videos,
         }]);
+      }else{
+        res.json([{
+          id: 0,
+          data: 'No data',
+        }]);
+      }
     }else{
       res.json([{
           id: 0,
