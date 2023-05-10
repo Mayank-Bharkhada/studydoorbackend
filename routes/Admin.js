469718675792ebc58a528d5d3ef3ab9fd3aa320f
Mr. Mayank Bharkhada <94800249+Mayank-Bharkhada@users.noreply.github.com>
2023-05-10 12:58:33 +0530

    Update Admin.js

--- routes/Admin.js
+++ routes/Admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const router = express.Router();
-const StudentModel = require('../schema/StudetSchema');
 const InstituteModel = require('../schema/InstituteSchema');
+const StudentModel = require('../schema/StudetSchema');
+const router = express.Router();
 
+const sendMSG = require('../module/Msg');
 //call through /api/User/Admin_login
 
 router.post("/Admin_login",async (req,res) => {
@@ -11,7 +12,7 @@ router.post("/Admin_login",async (req,res) => {
       const yourId = req.body.Id;
       console.log(yourId);
       const yourPassword = req.body.Password; //'ChmkjJ@11'
-      const hashedPassword = "$2b$10$gmxahITXdhvsptTPKo8vGuBj1FvgrKt2Q15DK0d6z9ktGEaGZxOoi";
+      const hashedPassword = '$2b$10$gmxahITXdhvsptTPKo8vGuBj1FvgrKt2Q15DK0d6z9ktGEaGZxOoi';
 
        if(yourId == "myname@gmail.com"){
         bcrypt.compare(yourPassword, hashedPassword, (err, result) => {
@@ -19,7 +20,7 @@ router.post("/Admin_login",async (req,res) => {
             console.log(result);
             res.json([{
               id : 1,
-              data : "Yours welcome Admin."
+              text : "Yours welcome Admin."
             }]);
           } else if (err) {
             console.log(err);
@@ -47,7 +48,8 @@ router.post("/Admin_login",async (req,res) => {
   });
   
   router.post('/requested_institute_data', async (req, res) => {
-    try {   
+    try {
+    
         const result = await InstituteModel.find({verificationRequest: 1 }).exec();
         if (result !== null) {
           res.send({
@@ -65,8 +67,7 @@ router.post("/Admin_login",async (req,res) => {
       res.status(500).send(error);
     }
   });
-
-
+  
   router.post('/requested_student_data', async (req, res) => {
     try {
     
@@ -87,8 +88,9 @@ router.post("/Admin_login",async (req,res) => {
       res.status(500).send(error);
     }
   });
+  
 
- //call through /api/User/Student_data
+  //call through /api/User/Student_data
 
 router.post('/Student_data_by_id', async (req, res) => {
   try {
@@ -114,15 +116,14 @@ router.post('/Student_data_by_id', async (req, res) => {
   }
 });
 
-
 router.post('/varify_institute_by_id', async (req, res) => {
   try {
       const instituteId = req.body.instituteId;
 
-      // Update the certificate document to confirm true
-      // const updatedInstitute = await CertificateModel.findByIdAndUpdate(certificateId, { confirm: true });
+      const instituteData = await InstituteModel.findOne({ _id: instituteId }).exec();
+      const Data = await sendMSG(instituteData.email, instituteData.number,"Your verification request is confirmed successfully now you can move to word to  use the application");
+      console.log(Data)
 
-      // Update the student document to set watch video and given quiz to null
       const updatedInstitute = await InstituteModel.updateOne({ _id:instituteId }, { $set: { verified : 1, verificationRequest : 0} });
 
       if (updatedInstitute.modifiedCount === 1) {
@@ -139,6 +140,10 @@ router.post('/varify_institute_by_id', async (req, res) => {
 router.post('/unvarify_institute_by_id', async (req, res) => {
   try {
       const instituteId = req.body.instituteId;
+
+      const instituteData = await InstituteModel.findOne({ _id: instituteId }).exec();
+      const Data = await sendMSG(instituteData.email, instituteData.number,"Your verification request is not confirmed try again by submitting the more reliable documents");
+      console.log(Data)
 
       // Update the certificate document to confirm true
       // const updatedInstitute = await CertificateModel.findByIdAndUpdate(certificateId, { confirm: true });
@@ -160,15 +165,24 @@ router.post('/unvarify_institute_by_id', async (req, res) => {
 router.post('/varify_student_by_id', async (req, res) => {
   try {
       const studentId = req.body.studentId;
+console.log(studentId)
 
-      // Update the student document 
-      const updatedInstitute = await StudentModel.updateOne({ _id: studentId }, { $set: { verified : 1, verificationRequest : 0} });
+const StudentData = await StudentModel.findOne({ _id: studentId }).exec();
+      const Data = await sendMSG(StudentData.email, StudentData.phone,"Your verification request is confirmed successfully now you can move to word to  use the application");
+      console.log(Data)
 
-      if (updatedInstitute.modifiedCount === 1) {
-      res.status(200).json({ success: true, message: "Institute varified successfully." });
-      }else{
-        res.status(201).json({ success: false, message: "Error no data" });
-      }
+      // Update the certificate document to confirm true
+      // const updatedInstitute = await CertificateModel.findByIdAndUpdate(certificateId, { confirm: true });
+
+      // Update the student document to set watch video and given quiz to null
+      const updatedStudent = await StudentModel.updateOne({ _id: studentId }, { $set: { verified : 1, verificationRequest : 0} });
+console.log(updatedStudent)
+
+      // if (updatedStudent.modifiedCount === 1) {
+      // res.status(200).json({ success: true, message: "Institute varified successfully." });
+      // }else{
+      //   res.status(201).json({ success: false, message: "Error no data" });
+      // }
   } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "Unable to approve certificate." });
@@ -179,11 +193,17 @@ router.post('/unvarify_student_by_id', async (req, res) => {
   try {
       const studentId = req.body.studentId;
 
+      const StudentData = await StudentModel.findOne({ _id: studentId }).exec();
+      const Data = await sendMSG(StudentData.email, StudentData.phone, "Your verification request is not  approved try  again with submitting reliable documentds");
+      console.log(Data)
 
-      // Update the student document
-      const updatedInstitute = await StudentModel.updateOne({ _id: studentId }, { $set: {verificationRequest : 0} });
+      // Update the certificate document to confirm true
+      // const updatedStudent = await CertificateModel.findByIdAndUpdate(certificateId, { confirm: true });
 
-      if (updatedInstitute.modifiedCount === 1) {
+      // Update the student document to set watch video and given quiz to null
+      const updatedStudent = await StudentModel.updateOne({ _id: studentId }, { $set: {verificationRequest : 0} });
+
+      if (updatedStudent.modifiedCount === 1) {
       res.status(200).json({ success: true, message: "Institute varified successfully." });
       }else{
         res.status(201).json({ success: false, message: "Error no data" });
@@ -193,6 +213,8 @@ router.post('/unvarify_student_by_id', async (req, res) => {
       res.status(500).json({ success: false, message: "Unable to approve certificate." });
   }
 });
+
+
 
 
 module.exports = router;
